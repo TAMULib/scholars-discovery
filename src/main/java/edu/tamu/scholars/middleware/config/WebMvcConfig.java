fad926857f17014a992ceadf84fc1a465be2518b
William Welling <wwelling@library.tamu.edu>
2021-10-13 15:54:29 -0500

    minor updates

--- src/main/java/edu/tamu/scholars/middleware/config/WebMvcConfig.java
+++ src/main/java/edu/tamu/scholars/middleware/config/WebMvcConfig.java
@@ -26,6 +26,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/api").setViewName("forward:/api/index.html");
+        WebMvcConfigurer.super.addViewControllers(registry);
     }
 
     @Override
