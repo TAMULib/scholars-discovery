ed1b2d198470dedff499486019a4aaa40a831761
William Welling <william_welling@harvard.edu>
2022-12-19 08:43:34 -0600

    Remove unused imports

--- src/main/java/edu/tamu/scholars/middleware/discovery/model/repo/impl/IndividualRepoImpl.java
+++ src/main/java/edu/tamu/scholars/middleware/discovery/model/repo/impl/IndividualRepoImpl.java
@@ -11,7 +11,6 @@ import static org.springframework.data.solr.core.query.Criteria.WILDCARD;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
