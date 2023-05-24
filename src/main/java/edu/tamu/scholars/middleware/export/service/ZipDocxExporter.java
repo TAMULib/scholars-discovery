a63ef6ebea7e7003d662d8153fb0d234bc930340
William Welling <wwelling@outlook.com>
2023-05-24 12:20:41 -0500

    Abstract docx redundancies

--- src/main/java/edu/tamu/scholars/middleware/export/service/ZipDocxExporter.java
+++ src/main/java/edu/tamu/scholars/middleware/export/service/ZipDocxExporter.java
@@ -6,9 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,80 +17,31 @@ import java.util.zip.ZipOutputStream;
 import javax.xml.bind.JAXBException;
 
 import org.docx4j.Docx4J;
-import org.docx4j.jaxb.Context;
-import org.docx4j.model.structure.SectionWrapper;
-import org.docx4j.openpackaging.contenttype.ContentType;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.PartName;
-import org.docx4j.openpackaging.parts.WordprocessingML.AltChunkType;
-import org.docx4j.openpackaging.parts.WordprocessingML.AlternativeFormatInputPart;
-import org.docx4j.openpackaging.parts.WordprocessingML.HeaderPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.docx4j.openpackaging.parts.WordprocessingML.NumberingDefinitionsPart;
-import org.docx4j.relationships.Relationship;
-import org.docx4j.wml.Body;
-import org.docx4j.wml.CTAltChunk;
-import org.docx4j.wml.Hdr;
-import org.docx4j.wml.HdrFtrRef;
-import org.docx4j.wml.HeaderReference;
-import org.docx4j.wml.ObjectFactory;
-import org.docx4j.wml.SectPr;
-import org.docx4j.wml.SectPr.PgMar;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import edu.tamu.scholars.middleware.discovery.argument.FilterArg;
 import edu.tamu.scholars.middleware.discovery.model.AbstractIndexDocument;
 import edu.tamu.scholars.middleware.discovery.model.Individual;
-import edu.tamu.scholars.middleware.discovery.model.repo.IndividualRepo;
 import edu.tamu.scholars.middleware.export.exception.ExportException;
-import edu.tamu.scholars.middleware.service.TemplateService;
 import edu.tamu.scholars.middleware.view.model.DisplayView;
 import edu.tamu.scholars.middleware.view.model.ExportFieldView;
 import edu.tamu.scholars.middleware.view.model.ExportView;
-import edu.tamu.scholars.middleware.view.model.repo.DisplayViewRepo;
 
 @Service
-public class ZipDocxExporter implements Exporter {
+public class ZipDocxExporter extends AbstractDocxExporter {
 
     private static final String TYPE = "zip";
 
     private static final String CONTENT_TYPE = "application/zip";
 
     private static final String CONTENT_DISPOSITION_TEMPLATE = "attachment; filename=%s.zip";
-
-    private static final ContentType HTML_CONTENT_TYPE = new ContentType("text/html");
-
-    private static final ObjectFactory WML_OBJECT_FACTORY = Context.getWmlObjectFactory();
-
-    @Autowired
-    private DisplayViewRepo displayViewRepo;
-
-    @Autowired
-    private IndividualRepo individualRepo;
-
-    @Autowired
-    private TemplateService handlebarsService;
-
-    @Autowired
-    private ObjectMapper mapper;
-
-    @Value("${vivo.base-url:http://localhost:8080/vivo}")
-    private String vivoUrl;
-
-    @Value("${ui.url:http://localhost:4200}")
-    private String uiUrl;
 
     @Override
     public String type() {
@@ -210,108 +158,6 @@ public class ZipDocxExporter implements Exporter {
         zos.closeEntry();
 
         return bytesRead;
-    }
-
-    private <D extends AbstractIndexDocument> ObjectNode processDocument(ObjectNode node, ExportView view) {
-        node.put("vivoUrl", vivoUrl);
-        node.put("uiUrl", uiUrl);
-        fetchLazyReferences(node, view.getLazyReferences());
-        return node;
-    }
-
-    private void fetchLazyReferences(ObjectNode node, List<ExportFieldView> lazyReferences) {
-        lazyReferences
-            .stream()
-            .filter(lazyReference -> node.hasNonNull(lazyReference.getField()))
-            .forEach(lazyReference -> {
-                JsonNode reference = node.get(lazyReference.getField());
-                List<String> ids = new ArrayList<String>();
-                if (reference.isArray()) {
-                    ids = StreamSupport.stream(reference.spliterator(), false).map(rn -> rn.get("id").asText()).collect(Collectors.toList());
-                } else {
-                    ids.add(reference.get("id").asText());
-                }
-                ArrayNode references = node.putArray(lazyReference.getField());
-                references.addAll((ArrayNode) mapper.valueToTree(fetchLazyReference(lazyReference, ids)));
-            });
-    }
-
-    private List<Individual> fetchLazyReference(ExportFieldView lazyReference, List<String> ids) {
-
-        List<FilterArg> filters = lazyReference.getFilters().stream().map(f -> {
-            return FilterArg.of(f.getField(), Optional.of(f.getValue()), Optional.of(f.getOpKey().getKey()), Optional.empty());
-        }).collect(Collectors.toList());
-
-        Sort sort = Sort.by(
-            lazyReference.getSort().stream().map(s -> {
-                return Order.by(s.getField()).with(s.getDirection());
-            }).collect(Collectors.toList())
-        );
-
-        int limit = lazyReference.getLimit();
-
-        return individualRepo.findByIdIn(ids, filters, sort, limit);
-    }
-
-    private void addMargin(final MainDocumentPart mainDocumentPart) {
-        final Body body = mainDocumentPart.getJaxbElement().getBody();
-        final SectPr sectPr = body.getSectPr();
-        final PgMar pgMar = sectPr.getPgMar();
-
-        pgMar.setLeft(BigInteger.valueOf(750));
-        pgMar.setRight(BigInteger.valueOf(750));
-        pgMar.setTop(BigInteger.valueOf(500));
-        pgMar.setBottom(BigInteger.valueOf(500));
-    }
-
-    private void addContent(final MainDocumentPart mainDocumentPart, String html) throws Docx4JException {
-        mainDocumentPart.addAltChunk(AltChunkType.Xhtml, html.getBytes(Charset.defaultCharset()));
-    }
-
-    private void createAndAddHeader(final WordprocessingMLPackage pkg, final String html) throws InvalidFormatException {
-        final HeaderPart headerPart = new HeaderPart(new PartName("/word/content-header.xml"));
-        pkg.getParts().put(headerPart);
-        final Relationship headerRel = pkg.getMainDocumentPart().addTargetPart(headerPart);
-        createAndAddHtmlHeader(headerPart, html);
-        final HeaderReference headerRef = WML_OBJECT_FACTORY.createHeaderReference();
-        headerRef.setId(headerRel.getId());
-        headerRef.setType(HdrFtrRef.DEFAULT);
-        final List<SectionWrapper> sections = pkg.getDocumentModel().getSections();
-        final SectPr lastSectPr = getLastSectionPart(pkg, sections);
-        lastSectPr.getEGHdrFtrReferences().add(headerRef);
-    }
-
-    private void createAndAddHtmlHeader(final HeaderPart headerPart, final String html) throws InvalidFormatException {
-        final Hdr hdr = WML_OBJECT_FACTORY.createHdr();
-        headerPart.setJaxbElement(hdr);
-        try {
-            final AlternativeFormatInputPart targetpart = createHeaderHtml(new PartName("/word/htmlheader.html"), html);
-            final Relationship rel = headerPart.addTargetPart(targetpart);
-            final CTAltChunk ac = WML_OBJECT_FACTORY.createCTAltChunk();
-            ac.setId(rel.getId());
-            hdr.getContent().add(ac);
-        } catch (final UnsupportedEncodingException e) {
-            throw new InvalidFormatException(e.getMessage(), e);
-        }
-    }
-
-    private AlternativeFormatInputPart createHeaderHtml(final PartName partName, final String html) throws InvalidFormatException, UnsupportedEncodingException {
-        final AlternativeFormatInputPart afiPart = new AlternativeFormatInputPart(partName);
-        afiPart.setBinaryData(html.getBytes(Charset.defaultCharset()));
-        afiPart.setContentType(HTML_CONTENT_TYPE);
-        return afiPart;
-    }
-
-    private SectPr getLastSectionPart(final WordprocessingMLPackage pkg, final List<SectionWrapper> sections) {
-        final SectionWrapper lastSect = sections.get(sections.size() - 1);
-        final SectPr currentSectPr = lastSect.getSectPr();
-        if (currentSectPr != null) {
-            return currentSectPr;
-        }
-        final SectPr sectPr = WML_OBJECT_FACTORY.createSectPr();
-        pkg.getMainDocumentPart().addObject(sectPr);
-        lastSect.setSectPr(sectPr);
-        return sectPr;
     }
 
 }
