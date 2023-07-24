039546eeedb2fafba666b364d404f996e5fe4e4a
William Welling <wwelling@library.tamu.edu>
2023-07-24 15:24:22 -0500

    Add container type enum to analytic view

--- src/main/java/edu/tamu/scholars/middleware/view/model/AnalyticView.java
+++ src/main/java/edu/tamu/scholars/middleware/view/model/AnalyticView.java
@@ -1,6 +1,9 @@
 package edu.tamu.scholars.middleware.view.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
 @Entity
@@ -9,8 +12,20 @@ public class AnalyticView extends CollectionView {
 
     private static final long serialVersionUID = 2912876591264398726L;
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ContainerType type;
+
     public AnalyticView() {
         super();
+    }
+
+    public ContainerType getType() {
+        return type;
+    }
+
+    public void setGraph(ContainerType type) {
+        this.type = type;
     }
 
 }
