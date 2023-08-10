ff0987576079f5b065cbfb73e1c480eb029b7159
William Welling <wwelling@library.tamu.edu>
2023-08-10 09:39:26 -0500

    Remove over encapsulation and correct separation of concern
    The helper classes in discovery package are violating common principles and are not providing abstraction to multiple implementations of their use.

--- src/main/java/edu/tamu/scholars/middleware/export/utility/FilenameUtility.java
+++ src/main/java/edu/tamu/scholars/middleware/export/utility/FilenameUtility.java
@@ -1,18 +1,24 @@
 package edu.tamu.scholars.middleware.export.utility;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import edu.tamu.scholars.middleware.discovery.model.AbstractIndexDocument;
 import edu.tamu.scholars.middleware.discovery.model.Individual;
-import edu.tamu.scholars.middleware.discovery.model.helper.IndividualHelper;
+import edu.tamu.scholars.middleware.discovery.model.Organization;
+import edu.tamu.scholars.middleware.discovery.model.Person;
 
 public class FilenameUtility {
 
     private final static String UNDERSCORE = "_";
+    private final static String NAME = "name";
+    private final static String LAST_NAME = "lastName";
+    private final static String FIRST_NAME = "firstName";
 
     private FilenameUtility() {
 
@@ -27,7 +33,27 @@ public class FilenameUtility {
     }
 
     public static String normalizeExportFilename(Individual individual) {
-        return localizeWhileUnderscoreReplaceSpaceWithUnderscore(IndividualHelper.as(individual).getLabel());
+        Map<String, Collection<Object>> content = individual.getContent();
+        StringBuilder label = new StringBuilder();
+
+        String clazz = individual.getClazz();
+
+        if (clazz.equals(Organization.class.getSimpleName()) && content.containsKey(NAME)) {
+            label.append(content.get(NAME).iterator().next())
+                .append(UNDERSCORE);
+        } else if (clazz.equals(Person.class.getSimpleName()) && content.containsKey(LAST_NAME)) {
+             label.append(content.get(LAST_NAME).iterator().next())
+                .append(UNDERSCORE);
+
+            if (content.containsKey(FIRST_NAME)) {
+                label.append(content.get(FIRST_NAME).iterator().next())
+                    .append(UNDERSCORE);
+            }
+        }
+
+        return localizeWhileUnderscoreReplaceSpaceWithUnderscore(label
+            .append(individual.getId())
+            .toString());
     }
 
     private static String localizeWhileUnderscoreReplaceSpaceWithUnderscore(String value) {
