376cf4428a785f76dfdaf0bc2a1bc5985311bbaf
William Welling <wwelling@library.tamu.edu>
2023-08-10 11:38:16 -0500

    Add initial data and analytics view
    The additional properties required for data and analytics UI/UX has yet to be determined.

---
+++ src/main/java/edu/tamu/scholars/middleware/view/model/DataAndAnalyticsView.java
@@ -0,0 +1,12 @@
+package edu.tamu.scholars.middleware.view.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "data_and_analytics_views")
+public class DataAndAnalyticsView extends CollectionView {
+
+    private static final long serialVersionUID = 2912876591264398726L;
+
+}
