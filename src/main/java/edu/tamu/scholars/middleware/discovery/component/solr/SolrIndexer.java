12cc445a6102295353c77fd67bb11a0102d0ec51
William Welling <wwelling@tamu.edu>
2023-08-15 11:06:01 -0500

    Fix copy field duplication

--- src/main/java/edu/tamu/scholars/middleware/discovery/component/solr/SolrIndexer.java
+++ src/main/java/edu/tamu/scholars/middleware/discovery/component/solr/SolrIndexer.java
@@ -62,17 +62,17 @@ public class SolrIndexer implements Indexer {
                 try {
                     SchemaRequest.AddField addFieldRequest = new SchemaRequest.AddField(fieldAttributes);
                     addFieldRequest.process(solrClient, COLLECTION);
+
+                    if (indexed.copyTo().length > 0) {
+                        try {
+                            SchemaRequest.AddCopyField addCopyFieldRequest = new SchemaRequest.AddCopyField(name, Arrays.asList(indexed.copyTo()));
+                            addCopyFieldRequest.process(solrClient, COLLECTION);
+                        } catch (Exception e) {
+                            logger.error("Failed to add copy field", e);
+                        }
+                    }
                 } catch (Exception e) {
                     logger.debug("Failed to add field", e);
-                }
-
-                if (indexed.copyTo().length > 0) {
-                    try {
-                        SchemaRequest.AddCopyField addCopyFieldRequest = new SchemaRequest.AddCopyField(name, Arrays.asList(indexed.copyTo()));
-                        addCopyFieldRequest.process(solrClient, COLLECTION);
-                    } catch (Exception e) {
-                        logger.debug("Failed to add copy field", e);
-                    }
                 }
             }
         }
