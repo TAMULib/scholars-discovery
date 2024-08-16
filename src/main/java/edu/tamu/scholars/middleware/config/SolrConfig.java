ae0174ee045f1a95f45ffa77689506173ca4b90e
William Welling <wwelling@tamu.edu>
2024-08-16 15:53:55 -0500

    Move caching Solr client to a class of its own

--- src/main/java/edu/tamu/scholars/middleware/config/SolrConfig.java
+++ src/main/java/edu/tamu/scholars/middleware/config/SolrConfig.java
@@ -1,28 +1,10 @@
 package edu.tamu.scholars.middleware.config;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.solr.client.solrj.SolrClient;
-import org.apache.solr.client.solrj.SolrRequest;
-import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.Http2SolrClient;
-import org.apache.solr.common.util.NamedList;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +13,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 
 import edu.tamu.scholars.middleware.config.model.IndexConfig;
-import edu.tamu.scholars.middleware.discovery.service.IndexService;
+import edu.tamu.scholars.middleware.service.CachingSolrClient;
 import edu.tamu.scholars.middleware.service.JwtTokenService;
 
 /**
@@ -56,130 +38,13 @@ public class SolrConfig {
     @Bean
     @Order(1)
     @Primary
-    public SolrClient solrServer(SolrClient solrClient, IndexConfig index, JwtTokenService tokenService)
-            throws IOException {
-
-        ObjectMapper mapper = new ObjectMapper();
-
-        File lookupTable = new File("src/test/resources/lookup_table.sd");
-
-        boolean fileValid = lookupTable.exists() && !lookupTable.isDirectory();
-
-        // <JWT, UUID>
-        final Map<String, String> map = fileValid ? mapper.readValue(lookupTable, new TypeReference<Map<String, String>>() { }) : new HashMap<>();
-
-        return new SolrClient() {
-
-            @Override
-            public void close() throws IOException {
-                solrClient.close();
-            }
-
-            @SuppressWarnings("unchecked")
-            @Override
-            public NamedList<Object> request(SolrRequest<?> request, String collection)
-                    throws SolrServerException, IOException {
-
-                if (index.isSchematize() || index.isOnStartup() || Objects.isNull(request)) {
-                    return solrClient.request(request, collection);
-                }
-
-                ObjectNode rootNode = mapper.createObjectNode();
-
-                rootNode.put("basePath", request.getBasePath());
-                rootNode.put("basicAuthUser", request.getBasicAuthUser());
-                rootNode.put("basicAuthPassword", request.getBasicAuthPassword());
-                rootNode.put("collection", request.getCollection());
-
-                if (Objects.nonNull(request.getHeaders())) {
-                    ObjectNode headers = mapper.createObjectNode();
-                    for (Map.Entry<String, String> header : request.getHeaders().entrySet()) {
-                        headers.put(header.getKey(), header.getValue());
-                    }
-                    rootNode.set("headers", headers);
-                }
-
-                rootNode.put("method", request.getMethod().toString());
-
-                if (Objects.nonNull(request.getParams()) && Objects.nonNull(request.getParams().jsonStr())) {
-                    ObjectNode params = (ObjectNode) mapper.readTree(request.getParams().jsonStr());
-                    rootNode.set("params", params);
-                }
-
-                rootNode.put("path", request.getPath());
-
-                if (Objects.nonNull(request.getPreferredNodes())) {
-                    ArrayNode preferredNodes = mapper.createArrayNode();
-                    for (String preferredNode : request.getPreferredNodes()) {
-                        preferredNodes.add(preferredNode);
-                    }
-                    rootNode.set("preferredNodes", preferredNodes);
-                }
-
-                if (Objects.nonNull(request.getQueryParams())) {
-                    ArrayNode queryParams = mapper.createArrayNode();
-                    for (String queryParam : request.getQueryParams()) {
-                        queryParams.add(queryParam);
-                    }
-                    rootNode.set("queryParams", queryParams);
-                }
-
-                rootNode.put("requestType", request.getRequestType());
-
-                // System.out.println("getResponseParser: " + request.getResponseParser());
-                // System.out.println("getUserPrincipal: " + request.getUserPrincipal());
-
-                Map<String, Object> claims = mapper.readValue(rootNode.toPrettyString(), new TypeReference<Map<String, Object>>() { });
-
-                String jwt = tokenService.createToken(collection, claims);
-
-                byte[] bytes = jwt.getBytes(StandardCharsets.UTF_8);
-                String uuid = UUID.nameUUIDFromBytes(bytes).toString();
-
-                NamedList<Object> response = null;
-
-                synchronized (map) {
-                    // if (map.containsKey(jwt)) {
-                    //     // remove file if file older than duration
-                    // }
-                    map.put(jwt, uuid);
-
-                    File directory = new File("src/test/resources/lookup_table");
-
-                    directory.mkdir();
-
-                    String filename = String.format("src/test/resources/lookup_table/%s", uuid);
-
-                    File file = new File(filename);
-
-                    if (file.exists()) {
-                        try (
-                            FileInputStream fileIn = new FileInputStream(filename);
-                            ObjectInputStream in = new ObjectInputStream(fileIn);
-                        ) {
-                            response = (NamedList<Object>) in.readObject();
-                        } catch (IOException | ClassNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        response = solrClient.request(request, collection);
-                        try (
-                            FileOutputStream fileOut = new FileOutputStream(filename);
-                            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-                        ) {
-                            out.writeObject(response);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-                        mapper.writerWithDefaultPrettyPrinter().writeValue(lookupTable, map);
-                    }
-                }
-
-                return response;
-            }
-
-        };
+    public SolrClient solrServer(
+        SolrClient solrClient,
+        JwtTokenService jwtTokenService,
+        IndexConfig index,
+        ObjectMapper objectMapper
+    ) throws IOException {
+        return new CachingSolrClient<SolrClient>(solrClient, jwtTokenService, index, objectMapper);
     }
 
 }
---
+++ src/main/java/edu/tamu/scholars/middleware/service/CachingSolrClient.java
@@ -0,0 +1,214 @@
+package edu.tamu.scholars.middleware.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.util.NamedList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.tamu.scholars.middleware.config.model.IndexConfig;
+
+public class CachingSolrClient<C extends SolrClient> extends SolrClient {
+
+    private static final Logger logger = LoggerFactory.getLogger("CachingSolrClient");
+
+    private static final String BASE_PATH = "basePath";
+    private static final String BASIC_AUTH_USER = "basicAuthUser";
+    private static final String BASIC_AUTH_PASSWORD = "basicAuthPassword";
+    private static final String COLLECTION = "collection";
+
+    private static final String HEADERS = "headers";
+    private static final String METHOD = "method";
+    private static final String PARAMS = "params";
+    private static final String PATH = "path";
+
+    private static final String PREFERRED_NODES = "preferredNodes";
+    private static final String QUERY_PARAMS = "queryParams";
+    private static final String REQUEST_TYPE = "requestType";
+
+    private final File lookupTable;
+
+    private final Map<String, String> map;
+
+    // there are different type of SolrClient
+    C client;
+
+    JwtTokenService jwtTokenService;
+
+    IndexConfig index;
+
+    ObjectMapper objectMapper;
+
+    public CachingSolrClient(C client, JwtTokenService jwtTokenService, IndexConfig index, ObjectMapper objectMapper) throws IOException {
+        this.lookupTable = new File("src/test/resources/lookup_table.sd");
+
+        // <JWT, UUID>
+        this.map = lookupTable.exists() && !lookupTable.isDirectory()
+            ? objectMapper.readValue(lookupTable, new TypeReference<ConcurrentHashMap<String, String>>() { })
+            : new ConcurrentHashMap<>();
+
+        this.client = client;
+        this.jwtTokenService = jwtTokenService;
+        this.index = index;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void close() throws IOException {
+        client.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public NamedList<Object> request(SolrRequest<?> request, String collection)
+            throws SolrServerException, IOException {
+
+        if (!request.getMethod().equals(org.apache.solr.client.solrj.SolrRequest.METHOD.GET) || index.isSchematize() || index.isOnStartup() || Objects.isNull(request)) {
+            return client.request(request, collection);
+        }
+
+        long start = System.currentTimeMillis();
+
+        ObjectNode rootNode = objectMapper.createObjectNode();
+
+        rootNode.put(BASE_PATH, request.getBasePath());
+        rootNode.put(BASIC_AUTH_USER, request.getBasicAuthUser());
+        rootNode.put(BASIC_AUTH_PASSWORD, request.getBasicAuthPassword());
+        rootNode.put(COLLECTION, request.getCollection());
+
+        if (Objects.nonNull(request.getHeaders())) {
+            ObjectNode headers = objectMapper.createObjectNode();
+            for (Map.Entry<String, String> header : request.getHeaders().entrySet()) {
+                headers.put(header.getKey(), header.getValue());
+            }
+            rootNode.set(HEADERS, headers);
+        }
+
+        rootNode.put(METHOD, request.getMethod().toString());
+
+        if (Objects.nonNull(request.getParams()) && Objects.nonNull(request.getParams().jsonStr())) {
+            ObjectNode params = (ObjectNode) objectMapper.readTree(request.getParams().jsonStr());
+            rootNode.set(PARAMS, params);
+        }
+
+        rootNode.put(PATH, request.getPath());
+
+        if (Objects.nonNull(request.getPreferredNodes())) {
+            ArrayNode preferredNodes = objectMapper.createArrayNode();
+            for (String preferredNode : request.getPreferredNodes()) {
+                preferredNodes.add(preferredNode);
+            }
+            rootNode.set(PREFERRED_NODES, preferredNodes);
+        }
+
+        if (Objects.nonNull(request.getQueryParams())) {
+            ArrayNode queryParams = objectMapper.createArrayNode();
+            for (String queryParam : request.getQueryParams()) {
+                queryParams.add(queryParam);
+            }
+            rootNode.set(QUERY_PARAMS, queryParams);
+        }
+
+        rootNode.put(REQUEST_TYPE, request.getRequestType());
+
+        // System.out.println("getResponseParser: " + request.getResponseParser());
+        // System.out.println("getUserPrincipal: " + request.getUserPrincipal());
+
+        logger.info("{}: {} seconds", "DESERIALIZE", (System.currentTimeMillis() - start) / (double) 1000);
+
+        long startClaimsToJWTUUID = System.currentTimeMillis();
+
+        Map<String, Object> claims = objectMapper.readValue(rootNode.toPrettyString(), new TypeReference<Map<String, Object>>() { });
+
+        String jwt = jwtTokenService.createToken(collection, claims);
+
+        byte[] bytes = jwt.getBytes(StandardCharsets.UTF_8);
+        String uuid = UUID.nameUUIDFromBytes(bytes).toString();
+
+        logger.info("{}: {} seconds", "CLAIMS_TO_JWT_UUID", (System.currentTimeMillis() - startClaimsToJWTUUID) / (double) 1000);
+
+        NamedList<Object> response = null;
+
+        synchronized (map) {
+            // if (map.containsKey(jwt)) {
+            // // remove file if file older than duration
+            // }
+
+            File directory = new File("src/test/resources/lookup_table");
+
+            directory.mkdir();
+
+            String filename = String.format("src/test/resources/lookup_table/%s", uuid);
+
+            File file = new File(filename);
+
+            long startFoundCache = System.currentTimeMillis();
+            long startQueryToSolr = System.currentTimeMillis();
+
+            if (file.exists()) {
+                startFoundCache = System.currentTimeMillis();
+
+                try (
+                    FileInputStream fileIn = new FileInputStream(filename);
+                    ObjectInputStream in = new ObjectInputStream(fileIn);
+                ) {
+                    response = (NamedList<Object>) in.readObject();
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                logger.info("{}:{}: {} seconds", uuid, "RESPONSE CACHED READ", (System.currentTimeMillis() - startFoundCache) / (double) 1000);
+            } else {
+                startQueryToSolr = System.currentTimeMillis();
+                response = client.request(request, collection);
+
+                // TODO: make asynchronous with thread pool
+                try (
+                    FileOutputStream fileOut = new FileOutputStream(filename);
+                    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                ) {
+                    out.writeObject(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                logger.info("{}:{}: {} seconds", uuid, "QUERY RESPONSE", (System.currentTimeMillis() - startQueryToSolr) / (double) 1000);
+
+                long startUdateLookupTable = System.currentTimeMillis();
+
+                // concurrent write, synchronize with file write
+                map.put(jwt, uuid);
+
+                // concurrent read, synchronize with file write
+                objectMapper.writerWithDefaultPrettyPrinter().writeValue(lookupTable, map);
+
+                logger.info("{}:{}: {} seconds", uuid, "UPDATE LOOKUP TABLE", (System.currentTimeMillis() - startUdateLookupTable) / (double) 1000);
+
+                logger.info("{}:{}:{} {}", uuid, "REQUEST", uuid, rootNode.toPrettyString());
+            }
+            logger.info("{}:{}: {} seconds", uuid, "ACTUAL RESPONSE", (System.currentTimeMillis() - start) / (double) 1000);
+        }
+
+        return response;
+    }
+
+}
