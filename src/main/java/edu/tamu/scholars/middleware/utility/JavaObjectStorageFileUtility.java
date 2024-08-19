1eaf4829dfce0a2cb73679602b713a754b80645a
William Welling <wwelling@tamu.edu>
2024-08-19 16:21:03 -0500

    Remove generate comments

--- src/main/java/edu/tamu/scholars/middleware/utility/JavaObjectStorageFileUtility.java
+++ src/main/java/edu/tamu/scholars/middleware/utility/JavaObjectStorageFileUtility.java
@@ -12,7 +12,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-// https://claude.ai/chat/38a46bde-f2c6-48b8-9a88-c4e286f55776
 public class JavaObjectStorageFileUtility {
 
     private JavaObjectStorageFileUtility() {
@@ -25,11 +24,9 @@ public class JavaObjectStorageFileUtility {
              ByteArrayOutputStream baos = new ByteArrayOutputStream();
              ObjectOutputStream oos = new ObjectOutputStream(baos)) {
 
-            // Serialize the object
             oos.writeObject(object);
             oos.flush();
 
-            // Write the serialized object to the file
             channel.write(ByteBuffer.wrap(baos.toByteArray()));
         }
     }
@@ -39,7 +36,6 @@ public class JavaObjectStorageFileUtility {
         try (FileChannel channel = FileChannel.open(path, StandardOpenOption.READ);
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 
-            // Read the file contents into a ByteArrayOutputStream
             ByteBuffer buffer = ByteBuffer.allocate(1024);
             while (channel.read(buffer) > 0) {
                 buffer.flip();
@@ -47,7 +43,6 @@ public class JavaObjectStorageFileUtility {
                 buffer.clear();
             }
 
-            // Deserialize the object
             try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()))) {
                 @SuppressWarnings("unchecked")
                 T object = (T) ois.readObject();
