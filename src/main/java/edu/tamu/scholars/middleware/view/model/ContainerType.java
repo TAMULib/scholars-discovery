b6ae9f0c15d9eb0249eb4bbdf01d78686143dee9
William Welling <wwelling@tamu.edu>
2025-01-31 14:35:43 -0600

    Add frequency graph container type enum

--- src/main/java/edu/tamu/scholars/middleware/view/model/ContainerType.java
+++ src/main/java/edu/tamu/scholars/middleware/view/model/ContainerType.java
@@ -4,5 +4,8 @@ package edu.tamu.scholars.middleware.view.model;
  * An initial enumeration for describing to the client which type of container a view is to be within.
  */
 public enum ContainerType {
-    ACADEMIC_AGE_GROUP, QUANTITY_DISTRIBUTION, PROFILE_SUMMARIES_EXPORT
+    ACADEMIC_AGE_GROUP,
+    QUANTITY_DISTRIBUTION,
+    FREQUENCY_GRAPH,
+    PROFILE_SUMMARIES_EXPORT
 }
