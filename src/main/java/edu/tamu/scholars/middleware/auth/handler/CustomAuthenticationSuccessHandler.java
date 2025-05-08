00bf62fd4839ff7efd164427b6c79254322ae47c
Rincy Mathew <rmathew@tamu.edu>
2025-05-08 16:39:33 -0500

    WIP - Added custom session configuration

--- src/main/java/edu/tamu/scholars/middleware/auth/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/edu/tamu/scholars/middleware/auth/handler/CustomAuthenticationSuccessHandler.java
@@ -9,7 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import edu.tamu.scholars.middleware.auth.model.User;
@@ -20,6 +24,7 @@ import edu.tamu.scholars.middleware.auth.model.User;
  */
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
     private ObjectMapper objectMapper;
 
     public CustomAuthenticationSuccessHandler(ObjectMapper objectMapper) {
@@ -33,8 +38,24 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         Authentication authentication
     ) throws IOException, ServletException {
         response.setContentType(APPLICATION_JSON_VALUE);
-        response.getOutputStream()
-            .write(objectMapper.writeValueAsBytes((User) authentication.getPrincipal()));
+
+        Saml2AuthenticatedPrincipal samlPrincipal = (Saml2AuthenticatedPrincipal) authentication.getPrincipal();
+
+        samlPrincipal.getAttributes().forEach((k, v) ->
+        logger.info(" \n\n CustomAuthenticationSuccessHandler onAuthenticationSuccess SAML Attributes: {} = {}", k, v)
+        );
+        String email = samlPrincipal.getFirstAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+        String firstName = samlPrincipal.getFirstAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname");
+        String lastName = samlPrincipal.getFirstAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname");
+
+        User user = new User();
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        logger.info(" \n\n CustomAuthenticationSuccessHandler onAuthenticationSuccess User email: {} ", user.getEmail());
+        logger.info(" \n\n CustomAuthenticationSuccessHandler onAuthenticationSuccess User getFirstName: {} ", user.getFirstName());
+        logger.info(" \n\n CustomAuthenticationSuccessHandler onAuthenticationSuccess User last name: {} ", user.getLastName());
+        response.getOutputStream().write(objectMapper.writeValueAsBytes(user));
     }
 
 }
--- src/main/java/edu/tamu/scholars/middleware/config/WebSecurityConfig.java
+++ src/main/java/edu/tamu/scholars/middleware/config/WebSecurityConfig.java
@@ -6,11 +6,17 @@ import static org.springframework.http.HttpMethod.PATCH;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpMethod.PUT;
 
+import java.lang.System.LoggerFinder;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Arrays;
 
+import javax.servlet.Filter;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
@@ -27,6 +33,8 @@ import org.springframework.security.core.token.TokenService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
+import org.springframework.security.saml2.provider.service.servlet.filter.Saml2WebSsoAuthenticationRequestFilter;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.savedrequest.NullRequestCache;
@@ -52,6 +60,8 @@ import edu.tamu.scholars.middleware.config.model.MiddlewareConfig;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${spring.profiles.active:default}")
     private String profile;
@@ -79,6 +89,7 @@ public class WebSecurityConfig {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        logger.info("\n\n\n WebSecurityConfig configureGlobal \n\n\n");
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
@@ -155,8 +166,15 @@ public class WebSecurityConfig {
         return serializer;
     }
 
+
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        logger.info("\n\n\n SecurityFilterChain setup started: \n");
+
+        http.getSharedObjects().forEach((key, value) ->
+            logger.info("\n\n\n Shared Object: {} = {}", key.getSimpleName(), value)
+        );
+        logger.info("\n\n\n-----\n\n\n");
         if (enableH2Console()) {
             // NOTE: permit all access to h2console
             http
@@ -164,109 +182,99 @@ public class WebSecurityConfig {
                     .frameOptions()
                         .sameOrigin();
         }
-        http
-            .authorizeRequests()
-                .expressionHandler(securityExpressionHandler)
 
-                .antMatchers(PATCH,
-                    "/dataAndAnalyticsViews/{id}",
-                    "/directoryViews/{id}",
-                    "/discoveryViews/{id}",
-                    "/displayViews/{id}",
-                    "/themes/{id}"
-                    )
-                    .hasRole("ADMIN")
+    http
+        .authorizeRequests()
+            .expressionHandler(securityExpressionHandler)
+            .antMatchers("/login/saml2/**", "/saml2/**")
+                .permitAll()
+            .antMatchers(PATCH, "/dataAndAnalyticsViews/{id}", "/directoryViews/{id}",
+                "/discoveryViews/{id}", "/displayViews/{id}", "/themes/{id}")
+                .hasRole("ADMIN")
+            .antMatchers(PATCH, "/users/{id}")
+                .hasRole("SUPER_ADMIN")
+            .antMatchers(POST, "/registration")
+                .permitAll()
+            .antMatchers(POST, "/dataAndAnalyticsViews/{id}", "/directoryViews/{id}",
+                "/discoveryViews/{id}", "/displayViews/{id}", "/themes/{id}")
+                .hasRole("ADMIN")
+            .antMatchers(POST, "/users/{id}")
+                .denyAll()
+            .antMatchers(PUT, "/registration")
+                .permitAll()
+            .antMatchers(PUT, "/dataAndAnalyticsViews/{id}", "/directoryViews/{id}",
+                "/discoveryViews/{id}", "/displayViews/{id}", "/themes/{id}")
+                .hasRole("ADMIN")
+            .antMatchers(PUT, "/users/{id}")
+                .denyAll()
+            .antMatchers(GET, "/user")
+                .hasRole("USER")
+            .antMatchers(GET, "/users", "/users/{id}", "/themes", "/themes/{id}")
+                .hasRole("ADMIN")
+            .antMatchers(DELETE, "/dataAndAnalyticsViews/{id}", "/directoryViews/{id}",
+                "/discoveryViews/{id}", "/displayViews/{id}", "/themes/{id}")
+                .hasRole("ADMIN")
+            .antMatchers(DELETE, "/users/{id}")
+                .hasRole("SUPER_ADMIN")
+            .anyRequest()
+                .permitAll();
 
-                .antMatchers(PATCH,
-                    "/users/{id}"
-                ).hasRole("SUPER_ADMIN")
+    http
+        .requestCache(requestCache -> requestCache
+            .requestCache(new NullRequestCache()));
+    http
+    .csrf(csrf -> csrf.disable())
+    .sessionManagement(session -> session
+        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+    )
+    .saml2Login(saml2 -> saml2
+        .loginPage("/saml2/authenticate/my-idp")
+        .successHandler(authenticationSuccessHandler())
+        .failureHandler(authenticationFailureHandler())
+        .permitAll()
+    );
+//         .authenticationRequestRepository(new StatelessSaml2AuthenticationRequestRepository())
 
-                .antMatchers(POST,
-                    "/registration")
-                    .permitAll()
+    http
+        .logout(logout -> logout
+            .deleteCookies("SESSION")
+            .invalidateHttpSession(true)
+            .logoutSuccessHandler(logoutSuccessHandler())
+            .permitAll());
 
-                .antMatchers(POST,
-                    "/dataAndAnalyticsViews/{id}",
-                    "/directoryViews/{id}",
-                    "/discoveryViews/{id}",
-                    "/displayViews/{id}",
-                    "/themes/{id}")
-                    .hasRole("ADMIN")
+    http
+        .exceptionHandling(handling -> handling
+            .authenticationEntryPoint(authenticationEntryPoint())
+            .accessDeniedHandler(accessDeniedHandler()));
 
-                .antMatchers(POST, "/users/{id}")
-                    .denyAll()
+    http
+        .csrf(csrf -> csrf
+            .disable());
 
-                .antMatchers(PUT, "/registration")
-                    .permitAll()
-
-                .antMatchers(PUT,
-                    "/dataAndAnalyticsViews/{id}",
-                    "/directoryViews/{id}",
-                    "/discoveryViews/{id}",
-                    "/displayViews/{id}",
-                    "/themes/{id}")
-                    .hasRole("ADMIN")
-
-                .antMatchers(PUT, "/users/{id}")
-                    .denyAll()
-
-                .antMatchers(GET, "/user")
-                    .hasRole("USER")
-
-                .antMatchers(GET,
-                    "/users",
-                    "/users/{id}",
-                    "/themes",
-                    "/themes/{id}")
-                    .hasRole("ADMIN")
-
-                .antMatchers(DELETE,
-                    "/dataAndAnalyticsViews/{id}",
-                    "/directoryViews/{id}",
-                    "/discoveryViews/{id}",
-                    "/displayViews/{id}",
-                    "/themes/{id}")
-                    .hasRole("ADMIN")
-
-                .antMatchers(DELETE, "/users/{id}")
-                    .hasRole("SUPER_ADMIN")
-
-                .anyRequest()
-                    .permitAll()
-
-            .and()
-                .formLogin()
-                    .successHandler(authenticationSuccessHandler())
-                    .failureHandler(authenticationFailureHandler())
-                        .permitAll()
-            .and()
-                .logout()
-                    .deleteCookies("SESSION")
-                    .invalidateHttpSession(true)
-                    .logoutSuccessHandler(logoutSuccessHandler())
-                        .permitAll()
-            .and()
-                .exceptionHandling()
-                    .authenticationEntryPoint(authenticationEntryPoint())
-                    .accessDeniedHandler(accessDeniedHandler())
-            .and()
-                .requestCache()
-                    .requestCache(nullRequestCache())
-            .and()
-                .cors()
-            .and()
-                .csrf()
-                    .disable();
-
-        http.sessionManagement()
+    http
+        .sessionManagement(management -> management
             .sessionFixation()
-                .migrateSession()
-            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+            .migrateSession()
+            .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
 
-        return http.build();
+    return http.build();
     }
 
+    /*
+
+    @Bean
+    public RelyingPartyRegistrationRepository relyingPartyRegistrationRepository() {
+        RelyingPartyRegistration registration = RelyingPartyRegistrations
+            .fromMetadataLocation("https://login.microsoftonline.com/{tenant-id}/federationmetadata/2007-06/federationmetadata.xml")
+            .registrationId("entra-id")
+            .build();
+        return new InMemoryRelyingPartyRegistrationRepository(registration);
+    }
+
+    */
+
     private CustomAuthenticationSuccessHandler authenticationSuccessHandler() {
+
         return new CustomAuthenticationSuccessHandler(objectMapper);
     }
 
