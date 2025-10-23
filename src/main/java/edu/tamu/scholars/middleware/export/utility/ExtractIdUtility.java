08ae55c4cf393cc4441bd10dc9fa7b9e53159593
Rincy Mathew <rmathew@tamu.edu>
2025-10-23 14:58:15 -0500

    Added a utility file - as part of PR suggestion

---
+++ src/main/java/edu/tamu/scholars/middleware/export/utility/ExtractIdUtility.java
@@ -0,0 +1,36 @@
+package edu.tamu.scholars.middleware.export.utility;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ */
+public class ExtractIdUtility {
+
+    private ExtractIdUtility() {
+
+    }
+
+    /**
+     * Converts list of string with '::' prefix into a list of ids.
+     */
+    public static List<String> extractIds(List<String> inputs) {
+        if (inputs == null || inputs.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return inputs.stream()
+                     .map(s -> s == null || s.isEmpty() ? null : (s.contains("::") ? s.split("::")[1] : s))
+                     .collect(Collectors.toList());
+    }
+
+    // /**
+    //  * Convenience method for a single string.
+    //  */
+    // public static String extractId(String input) {
+    //     if (input == null || input.isEmpty()) return null;
+    //     return input.contains("::") ? input.split("::")[1] : input;
+    // }
+
+}
